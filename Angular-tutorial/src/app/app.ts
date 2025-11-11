267b3ffcd2005597125b3142a6c0c08c22a172fc
yucaib <y.canfee@gmail.com>
2025-11-11 19:34:00 +0300

    Angular first-app

---
+++ Angular-tutorial/src/app/app.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {Home} from "./home/home";
+import {RouterModule} from "@angular/router";
+@Component({
+  selector: 'app-root',
+  imports: [Home, RouterModule],
+  template: `
+    <main>
+        <header class="brand-name"> <img class="brand-logo" src="assets/logo.svg" alt="logo" aria-hidden="true"></header>
+        
+        <section class="content">
+            <router-outlet></router-outlet>
+        </section>
+    </main>
+  `,
+  styleUrls: ['./app.css'],
+})
+export class App {
+  title = 'homes';
+}
---
+++ Angular-tutorial/src/app/home/home.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import {HousingLocation} from "../housing-location/housing-location";
+import{HousingService} from "../housing.service";
+import {HousingLocationInfo} from "../housinglocation";
+
+@Component({
+  selector: 'app-home',
+  imports: [HousingLocation],
+  template: `
+      <section>
+          <form>
+              <input type="text" placeholder="Filter by city">
+              <button class="primary" type="button">Search</button>
+          </form>
+      </section>
+      <section class="results">
+          @for (housingLocation of housingLocationList; track $index) {
+          <app-housing-location class="app-housing-location" [housingLocation]="housingLocation"></app-housing-location>
+          }
+      </section>
+  `,
+    styleUrls: ["./home.css"],
+})
+export class Home {
+    housingLocationList: HousingLocationInfo[] = [];
+
+    housingService: HousingService = inject(HousingService);
+
+    constructor() {
+        this.housingLocationList = this.housingService.getAllHousingLocations()
+    }
+
+}
+
+
